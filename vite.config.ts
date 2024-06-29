2862bf98b5860adaf160f23234983e3a90b848aa
spiriton <vanya.ershov.05@mail.ru>
2024-06-29 10:22:54 +0300

    Assigment-3

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,26 @@
 import { defineConfig } from 'vite'
+import  {resolve} from 'path'
+
+function generateAlias(absolutePath) {
+  // eslint-disable-next-line no-undef
+return resolve(process.cwd(), absolutePath);
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./Front-end-Web-Development-HomeWork/"
+  base: "./Front-end-Web-Development-HomeWork/",
+  resolve: {
+    alias: {
+        '@':generateAlias('src'),
+        '@utils': generateAlias('src/utils'),
+        '@config': generateAlias('src/config'),
+        '@pages': generateAlias('src/pages'),
+        '@components': generateAlias('src/components'),
+        '@assets': generateAlias('src/assets'),
+        '@recoil': generateAlias('src/recoil'),
+        '@shared': generateAlias('src/shared'),
+        '~@': generateAlias('src'),
+        '~@assets': generateAlias('src/assets'),
+    }
+}
 })
