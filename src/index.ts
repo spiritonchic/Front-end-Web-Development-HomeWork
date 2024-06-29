93e647a27566bd46b01f35287b9d80b7adacc739
spiriton <vanya.ershov.05@mail.ru>
2024-06-29 06:36:58 +0300

    Assigment-3

---
+++ src/index.ts
@@ -0,0 +1,54 @@
+import DOMPurify from "dompurify"
+
+interface comicsResponse {
+    img: string;
+    alt: string;
+    title: string;
+    year: number;
+    month: number;
+    day: number;
+}
+
+const getID = async () => {
+    const params: URLSearchParams = new URLSearchParams({email: 'i.ershov@innopolis.university'})
+    const url: string = `https://fwd.innopolis.university/api/hw2?${params}`
+    const response: Response = await fetch(url)
+    const data: number = await response.json()
+    return data;
+}
+
+const getComicsData = async (id: number) => {
+    const params = new URLSearchParams({id: String(id)})
+    const url = `https://fwd.innopolis.university/api/comic?${params}`
+    const response = await fetch(url)
+    const data = response.json()
+    return data;
+}
+
+const secureData = (data: string) => {
+    return DOMPurify.sanitize(data, {
+        ALLOWED_TAGS: [],
+        ALLOWED_ATTR: []
+    })
+}
+
+const changeData = async (data: comicsResponse) => {
+    const title: HTMLParagraphElement = document.getElementById("comic-title") as HTMLParagraphElement
+    const img: HTMLImageElement = document.getElementById("comic-img") as HTMLImageElement
+    const date: HTMLParagraphElement = document.getElementById("comic-date") as HTMLParagraphElement
+    img.src = data.img
+    img.alt = secureData(data.alt)
+    title.textContent = secureData(data.title)
+    date.textContent = new Date(Number(secureData(String(data.year))), Number(secureData(String(data.month))) - 1, Number(secureData(String(data.day)))).toLocaleDateString()
+
+}
+
+const id: number = await getID();
+
+const data = await getComicsData(id);
+
+console.log(3);
+
+changeData(data)
+
+console.log(4);
