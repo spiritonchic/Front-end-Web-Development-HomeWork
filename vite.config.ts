5c84a979d21f7d47609cad1e34084a83ff864c66
spiriton <vanya.ershov.05@mail.ru>
2024-06-29 11:34:58 +0300

    Assigment-3

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
+import typescript from '@rollup/plugin-typescript';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Front-end-Web-Development-HomeWork/"
+  base: "/Front-end-Web-Development-HomeWork/",
+  plugins: [typescript(), /* Other plugins */],
 })
