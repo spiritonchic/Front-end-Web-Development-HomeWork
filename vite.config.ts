77a7739652961fa94e544ddfc561d248eb02145f
spiriton <vanya.ershov.05@mail.ru>
2024-06-29 09:45:15 +0300

    Assigment-3

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Front-end-Web-Development-HomeWork/",
+  base: "/",
   build: {
     rollupOptions: {
       input: {
