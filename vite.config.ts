3b9d26a8e0430d8ee82129c178559c1d320be74e
spiriton <vanya.ershov.05@mail.ru>
2024-06-29 10:50:04 +0300

    Assigment-3

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,18 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Front-end-Web-Development-HomeWork/"
+  base: "/Front-end-Web-Development-HomeWork/",
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'index.html'),
+        home: resolve(__dirname, 'home.html'),
+        projects: resolve(__dirname, 'projects.html'),
+        skills: resolve(__dirname, 'skills.html'),
+        comics: resolve(__dirname, 'comics.html')
+      }
+    }
+  }
 })
